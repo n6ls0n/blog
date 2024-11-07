14905d06f00dd7b00fd06c35c42960f6a296721b
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-11-07 14:39:45 -0500

    Upgraded to Rails 7.2.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_190825) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_07_193747) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
